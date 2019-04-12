51c218c1bc34fef154db7853d918211d328171ab
Unknown <laurelenn-sangare@laposte.net>
2019-04-12 12:07:56 +0200

    Model Citations
    Init First Functions Model Citation (get - empty)

---
+++ model/ModelCitations.php
@@ -0,0 +1,36 @@
+<?php
+require 'MyPDO.citatimac.include.php';
+
+
+
+  function getCitation(){
+
+  }
+
+  function getIdCitation(){
+
+  }
+
+  function getContenuCitation(){
+
+  }
+
+  function getDateCitation(){
+
+  }
+
+  function getAuteurCitation(){
+
+  }
+
+  function getLikesCitation(){
+
+  }
+
+  function getTypeAuteurCitation(){
+
+  }
+
+
+
+?>
--- model/MyPDO.citatimac.include.php
+++ model/MyPDO.citatimac.include.php
@@ -3,3 +3,5 @@
 require_once 'MyPDO.class.php';
 
 MyPDO::setConfiguration('mysql:host=localhost;dbname=akohlmul_db;charset=utf8', 'akohlmul', 'BDBwait2see');
+
+?>
