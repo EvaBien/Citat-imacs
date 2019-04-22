f3bcdae7f60fde985028c471d2c633c80f5a6e40
EvaBien <44610935+EvaBien@users.noreply.github.com>
2019-04-22 10:40:48 +0200

    Add modelSignalement.php

---
+++ model/ModelSignalement.php
@@ -0,0 +1,29 @@
+<?php
+require 'MyPDO.citatimac.include.php';
+
+  function getSignalement(){
+
+  }
+
+  function getIdSignalement(){
+
+  }
+
+  function getIdCitation(){
+
+  }
+
+  function getTypeSignalement(){
+
+  }
+  
+  function getMessageSignalement(){
+
+  }
+
+  function getStatutSignalement(){
+
+  }
+
+
+?>
