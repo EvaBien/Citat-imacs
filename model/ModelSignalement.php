cc3c6a5bfe90c48130bcdd602a6891589abb231a
EvaBien <eva.benharira@gmail.com>
2019-04-24 17:32:11 +0200

    Completed modelSignalement
    Add class Signalement and Setters

--- model/ModelSignalement.php
+++ model/ModelSignalement.php
@@ -1,5 +1,22 @@
 <?php
 require 'MyPDO.citatimac.include.php';
+date_default_timezone_set('UTC');
+
+class signalement {
+    private $id;
+    private $typeSignalement;
+    private $messageSignalement;
+    private $statutSignalement;
+    public function __construct($typeSignalement, $messageSignalement, $statutSignalement){
+                                  // Id auto
+      $this->typeSignalement=$typeSignalement;
+      $this->getMessageSignalement=new ; // A récupérer dans ce qu'écrit l'utilisateur ?
+      $this->statutSignalement=$statutSignalement;
+      $this->idCitation=$idCitation; // Lier à la table Citation
+    }
+}
+
+///////////////////////FUNCTIONS GETTER////////////////////////
 
   function getSignalement(){
 
@@ -24,6 +41,22 @@ require 'MyPDO.citatimac.include.php';
   function getStatutSignalement(){
 
   }
+
+  ///////////////////////FUNCTIONS SETTER///////////////////////
+  
+    function setSignalement(){
+    }
+    function setIdSignalement(){
+    }
+    function setIdCitation(){
+    }
+    function setTypeSignalement(){
+    }
+    function setMessageSignalement(){
+    }
+    function setStatutSignalement(){
+    }
+
 
 
 ?>
