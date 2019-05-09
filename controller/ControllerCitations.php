c6f3018a3f6abb7cb2e63187d901d0adac2b6a4d
Unknown <laurelenn-sangare@laposte.net>
2019-05-09 14:28:43 +0200

    Add ReadAllCitations()

--- controller/ControllerCitations.php
+++ controller/ControllerCitations.php
@@ -45,14 +45,94 @@ public function apiCreateCitation(HTTPRequest $request)
 //////////////////////////////////////////////////////////////
 
 
-// GET ALL CITATIONS //
+////////////////////// GET ALL CITATIONS ///////////////////
 public function apiGetAllCitations(HTTPRequest $request){
-  $citations[]= '';
+  // check HTTP method //
+  $method = strtolower($_SERVER['REQUEST_METHOD']);
 
+  if ($method !== 'get') {
+  	http_response_code(405);
+  	echo json_encode(array('message' => 'This method is not allowed.'));
+  	exit();
+  }
+  // response status
+  http_response_code(200);
+
+  ////SEARCH CITATION IN DB ////
+  $citations = array();
+  $stmt = MyPDO::getInstance()->prepare(`
+  SELECT *
+  FROM S2_Citations
+  ORDER BY dateCitation;
+    `);
+  $stmt->execute();
+
+  while (($row = $stmt->fetch()) !== false) {
+  	array_push($citations, $row); // Ajoute chaque citation au tableau citations
+  }
+
+  foreach ($citations as $key => $citation) { // On va chercher les tags et le typeAuteur
+  $typeAuteur='';
+  $tags = array();
+
+
+////SEARCH TYPEAUTEUR IN DB ////
+	$stmt = MyPDO::getInstance()->prepare(<<<SQL
+		SELECT S2_TypesAuteur.nomTypeAuteur FROM `S2_TypesAuteur`
+		INNER JOIN S2_Citations ON S2_Citations.idTypeAuteur = S2_TypesAuteur.idTypeAuteur
+		WHERE S2_Citations.idCitation = :idcitation;
+SQL
+	);
+	$stmt->execute(['idcitation'=>$citation['idCitation']]);
+	while (($row = $stmt->fetch()) !== false) {
+		$typeAuteur=$row['nomTypeAuteur'];
+	}
+
+////SEARCH TAGS IN DB ////
+  $stmt = MyPDO::getInstance()->prepare(<<<SQL
+		SELECT S2_Tags.nomTag FROM `S2_Tags`
+		INNER JOIN S2_TagCitations ON S2_TagCitation.idTag = S2_Tags.idTag
+		INNER JOIN S2_Citations ON S2_Citations.idCitation = S2_TagCitation.idCitation
+		WHERE S2_TagCitation.idCitation = :idcitation;
+SQL
+	);
+	$stmt->execute(['idcitation'=>$citation['idCitation']]);
+	while (($row = $stmt->fetch()) !== false) {
+		array_push($tags, $row['nomTag']);
+	}
+
+  // RANGER DANS LES CLES DE CITATION + ENCODER EN JSON//
+  $citations[$key]['typeAuteur'] = $typeAuteur;
+  $citations[$key]['tags'] = $tags;
+
+  echo json_encode($citations);
+
+  exit();
 
 }
 
 // GET CITATION BY ID //
+
+public function apiGetCitationById(HttpRequest $request){
+  // check HTTP method //
+$method = strtolower($_SERVER['REQUEST_METHOD']);
+if ($method !== 'get') {
+    http_response_code(405);
+    echo json_encode(array('message' => 'This method is not allowed.'));
+    exit();
+}
+
+// VERIF//
+if (isset($_GET['id'])) {
+    $request['id'] = $_GET['id'];
+}
+else {
+	http_response_code(404);
+  echo json_encode("No ID provided.");
+  exit();
+}
+
+}
 
 // GET CITATION BY TAGS //
 
@@ -68,7 +148,7 @@ public function apiGetAllCitations(HTTPRequest $request){
 
 public static function apiUpdateCitation(HTTPRequest $request)
   {
-    $queryStmt = "UPDATE Casts SET firstname = :name WHERE id = :id";
+    $queryStmt = "UPDATE Citations SET firstname = :name WHERE id = :id";
 
     $stmt = MyPDO::getInstance()->prepare($queryStmt);
     $stmt->execute(
@@ -91,7 +171,7 @@ public static function apiUpdateCitation(HTTPRequest $request)
 
 public static function apiDeleteCitation(HTTPRequest $request)
   {
-    $queryStmt = "DELETE FROM Casts WHERE id = ?";
+    $queryStmt = "DELETE FROM Citation WHERE id = $request[id]";
 
     $stmt = MyPDO::getInstance()->prepare($queryStmt);
     $stmt->execute(
