2019bd2af68d2cfcb6dc31201250e12db8e3580a
EvaBien <eva.benharira@gmail.com>
2019-05-11 17:28:45 +0200

    Add newSignalement to DB

--- controller/ControllerSignalements.php
+++ controller/ControllerSignalements.php
@@ -25,8 +25,23 @@ public function apiCreateSignalement(HTTPRequest $query)
   $signalement = new signalement($query['typeSignalement'],$query['messageSignalement'],$query['statutSignalement'],$query['idCitation']);
 
   ////// ADD TO DB //////
+  $queryStmt = "INSERT INTO signalement (typeSignalement, messageSignalement, statutSignalement, idCitation) VALUES (?, ?, ?, ?);"
 
+  $stmt = MyPDO::getInstance()->prepare($queryStmt);
+
+  $stmt->bindValue(1, $signalement->getTypeSignalement());
+  $stmt->bindValue(2, $signalement->getMessageSignalement());
+  $stmt->bindValue(3, $signalement->getStatutSignalement());
+  $stmt->bindValue(4, $signalement->getIdCitation());
+
+  $queryStatus = $stmt->execute();
+
+  if ($queryStatus === false) {
+    self::throwAnError();
+  } else {
+    $signalement->id = MyPDO::getInstance()->lastInsertId();
   }
+}
 
 
   ////////////////////////////////////////////////////////////////
