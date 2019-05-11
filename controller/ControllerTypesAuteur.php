5c037eed47cc5397c00929009bf8dea1eeeac46f
EvaBien <eva.benharira@gmail.com>
2019-05-11 16:11:56 +0200

    Api get all typesAuteur completed

--- controller/ControllerTypesAuteur.php
+++ controller/ControllerTypesAuteur.php
@@ -10,6 +10,23 @@ require '../model/ModelTypesAuteur.php';
 ///////////////////////// GET ALL TYPES AUTEUR //////////////////////
 // Pour mettre les champs dynamiquement dans search
 public function apiGetAllTypeAuteurs(HTTPRequest $request){
+   // check HTTP method //
+  $method = strtolower($_SERVER['REQUEST_METHOD']);
+
+  if ($method !== 'get') {
+  	http_response_code(405);
+  	echo json_encode(array('message' => 'This method is not allowed.'));
+  	exit();
+  }
+  // response status
+  http_response_code(200);
+
+  ////SEARCH TYPE AUTEUR IN DB ////
+  $typeAuteur = array();
+  $queryStmt = "SELECT * FROM S2_TypesAuteur;"
+  $stmt = MyPDO::getInstance()->prepare($queryStmt);
+  $stmt->execute();
+  exit();
 }
 
 
