f5f316c4f8f32144f14ba28163c6643eb9070389
Unknown <laurelenn-sangare@laposte.net>
2019-05-19 15:36:34 +0200

    Edit Route

--- api/Route/routeur.php
+++ api/Route/routeur.php
@@ -35,69 +35,69 @@ switch ($url) {
   apiGetAllCitations($request); // GET ALL
   break;
   /////CITATIONS//////
-  case '~lsangare/citacimac/citations/New' : // Create
+  case '/~lsangare/citacimac/citations/New' : // Create
   apiCreateCitation($request);
   break;
-  case '~lsangare/citacimac/citations/All' :
+  case '/~lsangare/citacimac/citations/All' :
   apiGetAllCitations($request); // GET ALL
   break;
-  case '~lsangare/citacimac/citations/Id' :
+  case '/~lsangare/citacimac/citations/Id' :
   apiGetCitationById($request); // Get By Id
   break;
-  case '~lsangare/citacimac/citations/Tags' :
+  case '/~lsangare/citacimac/citations/Tags' :
   apiGetCitationByTags($request); // Get by Tags
   break;
-  case '~lsangare/citacimac/citations/Keyword' :
+  case '/~lsangare/citacimac/citations/Keyword' :
   apiGetCitationByKeyword($request); // Get by keyword
   break;
-  case '~lsangare/citacimac/citations/Typesauteur' :
+  case '/~lsangare/citacimac/citations/Typesauteur' :
   apiGetCitationByTypeAuteur($request); // Get by typesAuteur
   break;
-  case '~lsangare/citacimac/citations/TagsKeyword' :
+  case '/~lsangare/citacimac/citations/TagsKeyword' :
   apiGetCitationByTagsAndKeyword($request); // Get by tags & keywords
   break;
-  case '~lsangare/citacimac/citations/TypesauteurKeyword' :
+  case '/~lsangare/citacimac/citations/TypesauteurKeyword' :
   apiGetCitationByTypeAuteurAndKeyword($request); // Get by typesAuteur & keyword
   break;
-  case '~lsangare/citacimac/citations/TypesauteurTags' :
+  case '/~lsangare/citacimac/citations/TypesauteurTags' :
   apiGetCitationByTypeAuteurAndTags($request); // Get by typesAuteur & Tags
   break;
-  case '~lsangare/citacimac/citations/Allfactors' :
+  case '/~lsangare/citacimac/citations/Allfactors' :
   apiGetCitationByAll($request); // Get by typesAut+keyword+tags
   break;
-  case '~lsangare/citacimac/citations/Update' :
+  case '/~lsangare/citacimac/citations/Update' :
   apiUpdateCitation($request); // Update
   break;
-  case '~lsangare/citacimac/citations/Delete' :
+  case '/~lsangare/citacimac/citations/Delete' :
   apiDeleteCitation($request); // Delete
   break;
-  case '~lsangare/citacimac/citations/GetLikes' :
+  case '/~lsangare/citacimac/citations/GetLikes' :
   getCitationLikes($request); // get citation likes
   break;
-  case '~lsangare/citacimac/citations/UpdateLikes' :
+  case '/~lsangare/citacimac/citations/UpdateLikes' :
   updateCitationLikes($request); // Update citation likes
   break;
   ///////////// ALL TAGS, TYPESAUTEURS & TYPESSIGNALEMENTS /////////////
-  case '~lsangare/citacimac/tags/All' :
+  case '/~lsangare/citacimac/tags/All' :
   apiGetAllTags($request); // Get All Tags
   break;
-  case '~lsangare/citacimac/typesAuteur/All' :
+  case '/~lsangare/citacimac/typesAuteur/All' :
   apiGetAllTypeAuteurs($request); // Get AlL TypesAuteur
   break;
-  case '~lsangare/citacimac/typesSignalement/All' :
+  case '/~lsangare/citacimac/typesSignalement/All' :
   apiGetAllTypesSignalement($request); // GetAllTypesSignalement
   break;
   //////////////// SIGNALEMENTS /////////
-  case '~lsangare/citacimac/signalement/New' :
+  case '/~lsangare/citacimac/signalement/New' :
   apiCreateSignalement($request); // Create Signalement
   break;
-  case '~lsangare/citacimac/signalement/Id' :
+  case '/~lsangare/citacimac/signalement/Id' :
   apiGetSignalementById($request); // Get By id
   break;
-  case '~lsangare/citacimac/signalement/Update' :
+  case '/~lsangare/citacimac/signalement/Update' :
   apiUpdateSignalement($request); // Update signalement
   break;
-  case '~lsangare/citacimac/signalement/Send' :
+  case '/~lsangare/citacimac/signalement/Send' :
   sendMailSignalement($request); // Send Mail
   break;
   default:
