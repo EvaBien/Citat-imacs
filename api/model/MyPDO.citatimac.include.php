6965d2aeedf320ec6be30dacec94a18671d523c0
Unknown <laurelenn-sangare@laposte.net>
2019-05-20 19:15:18 +0200

    Change localhost

--- api/model/MyPDO.citatimac.include.php
+++ api/model/MyPDO.citatimac.include.php
@@ -2,6 +2,6 @@
 
 require_once 'MyPDO.class.php';
 
-MyPDO::setConfiguration('mysql:host=localhost;dbname=akohlmul_db;charset=utf8', 'akohlmul', 'BDBwait2see');
+MyPDO::setConfiguration('mysql:host=etudiant.u-pem.fr;dbname=akohlmul_db;charset=utf8', 'akohlmul', 'BDBwait2see');
 
 ?>
