9d6c01ba0f96cabf8ee1169f3e48f689cd092b58
Unknown <laurelenn-sangare@laposte.net>
2019-05-23 01:04:27 +0200

    Nothing

--- js/search.js
+++ js/search.js
@@ -137,7 +137,7 @@ function addCitation(){
     data['idTypeAuteur'] = type_auteur;
     formData.append("idTypeAuteur", type_auteur);
   }
-
+lo
   if(tagsChecked){
     data['tagsCitation'] = tagsChecked;
     formData.append("tagsCitation",tagsChecked);
@@ -151,8 +151,8 @@ function addCitation(){
 		body: formData})
     .then( response => response.json() )
 		.then( data => {
-      alert("Citation créée ! \n Redirection...");
-      window.location.reload();
+      window.alert("Citation créée ! \n Redirection...");
+      // window.location.reload();
     })
     .catch( error => {
       window.alert(error);
