10002a3e2b1a1173d47b01e022ce195bd4fa0c31
Unknown <laurelenn-sangare@laposte.net>
2019-05-25 16:39:17 +0200

    Change route mail
    Not dynamic but works

--- api/controller/ControllerSignalements.php
+++ api/controller/ControllerSignalements.php
@@ -147,7 +147,7 @@ function sendMailSignalement($query, $idSignalement){
               <body>
               <p>".$query['messageSignal']."</p>
 
-              <a href=\"./admin.html?id=".$idSignalement."\">Cliquez ici pour corriger la citation</a>
+              <a href=\"perso-etudiant.u-pem.fr/~akohlmul/citatimac/admin.html?id=".$idSignalement."\">Cliquez ici pour corriger la citation</a>
               </body>
               </html>
               ";
