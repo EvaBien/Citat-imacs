fa4be146ac31c8d7c584d70b4a42518241dc547c
Unknown <laurelenn-sangare@laposte.net>
2019-05-25 14:12:12 +0200

    Finished version without Localhost in myPDO
    Retour à la version précédente si ne change rien

--- api/model/MyPDO.citatimac.include.php
+++ api/model/MyPDO.citatimac.include.php
@@ -1,10 +1,7 @@
 <?php
 
 require_once 'MyPDO.class.php';
-if ($_SERVER['SERVER_NAME']=="localhost"){
-  MyPDO::setConfiguration('mysql:host=localhost;dbname=citatimac;charset=utf8', 'root', '');
 
-} else {
   MyPDO::setConfiguration('mysql:host=etudiant.u-pem.fr;dbname=akohlmul_db;charset=utf8', 'akohlmul', 'BDBwait2see');
-}
+
 ?>
