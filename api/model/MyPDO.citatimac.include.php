5679195f1c687e0dee4a6d7f3a4c769bc00de809
Unknown <laurelenn-sangare@laposte.net>
2019-05-25 15:05:28 +0200

    Change Host

--- api/model/MyPDO.citatimac.include.php
+++ api/model/MyPDO.citatimac.include.php
@@ -5,6 +5,6 @@ if ($_SERVER['SERVER_NAME']=="localhost"){
   MyPDO::setConfiguration('mysql:host=localhost;dbname=citatimac;charset=utf8', 'root', '');
 
 } else {
-  MyPDO::setConfiguration('mysql:host=etudiant.u-pem.fr;dbname=akohlmul_db;charset=utf8', 'akohlmul', 'BDBwait2see');
+  MyPDO::setConfiguration('mysql:host=sqletud.u-pem.fr;dbname=akohlmul_db;charset=utf8', 'akohlmul', 'BDBwait2see');
 }
 ?>
